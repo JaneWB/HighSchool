fe014754d8c3dcda2781a2f607cec899b8fab472
JaneWB <brownjannick@att.net>
2018-10-05 23:15:54 -0500

    Week 13-2 High Shool

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace HighSchool
 {
@@ -6,7 +8,44 @@ namespace HighSchool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> S = new List<Student>()
+            {
+                new Student(){LastNameOfStudent = "Brown", FirstNameOfStudent = "Jane",
+                CurrentGpa = 4, Grade = 12}, 
+                new Student(){LastNameOfStudent = "Blue", FirstNameOfStudent = "Greg",
+                    CurrentGpa = 2, Grade = 11},
+                new Student(){LastNameOfStudent = "William", FirstNameOfStudent = "Erik",
+                    CurrentGpa = 4, Grade = 5},
+                new Student(){LastNameOfStudent = "James", FirstNameOfStudent = "Steve",
+                    CurrentGpa = 4, Grade = 2},
+                new Student(){LastNameOfStudent = "Color", FirstNameOfStudent = "Courtney",
+                    CurrentGpa = 4, Grade = 10},
+                new Student(){LastNameOfStudent = "Krump", FirstNameOfStudent = "Luke",
+                    CurrentGpa = 2, Grade = 10},
+                new Student(){LastNameOfStudent = "Jacob", FirstNameOfStudent = "Llew",
+                    CurrentGpa = 4, Grade = 1},
+                new Student(){LastNameOfStudent = "Krass", FirstNameOfStudent = "Lydia",
+                    CurrentGpa = 4, Grade = 9},
+                new Student(){LastNameOfStudent = "Pops", FirstNameOfStudent = "Jacob",
+                    CurrentGpa = 4, Grade = 2},
+                new Student(){LastNameOfStudent = "Lovely", FirstNameOfStudent = "Sue",
+                    CurrentGpa = 3, Grade = 12},
+            };
+
+            var studentList = S.Where(p => p.Grade >= 9 && p.CurrentGpa >= 3).OrderByDescending(p => p.Grade).ThenByDescending(p => p.CurrentGpa).
+                ThenBy(p => p.LastNameOfStudent).ThenBy(p => p.FirstNameOfStudent); 
+            //OrderByDescending(p => p.LastNameOfStudent);
+            //studentList = studentList
+
+            foreach (var item in studentList)
+            {
+                
+                Console.WriteLine("{0}, {1}  GPA: {2} Grade: {3}", item.LastNameOfStudent, item.FirstNameOfStudent, 
+                    item.CurrentGpa, item.Grade);
+
+            }
+            Console.ReadLine();
+
         }
     }
 }
---
+++ HighSchool/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HighSchool
+{
+    class Student
+    {
+        //public string lastNameOfStudent;
+        //public string firstNameOfStudent;
+        //public int currentGpa;
+        //public int grade;
+
+        public string LastNameOfStudent { get; set; }
+        public string FirstNameOfStudent { get; set; }
+        public int CurrentGpa { get; set; }
+        public int Grade { get; set; }
+
+
+
+    }
+}
